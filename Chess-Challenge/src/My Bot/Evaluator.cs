e3cd9b267df0d8b2bbbd4199ba0fd1de962b06ca
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-15 11:44:41 +0800

    Simplify psqt Tokens: 437

--- Chess-Challenge/src/My Bot/Evaluator.cs
+++ Chess-Challenge/src/My Bot/Evaluator.cs
@@ -1,6 +1,5 @@
 using ChessChallenge.API;
 using System;
-using System.Drawing;
 
 public static class Values
 {
@@ -8,16 +7,16 @@ public static class Values
     public static readonly int[] pieceValues = { 0, 126, 825, 781, 1276, 2538, 40000 };
     
     // Psqt
-    public static readonly int[,] knightPsqt = new int[8,4]
+    public static readonly int[] knightPsqt = new int[32]
     {
-        {-136, -78, -61, -47},
-        {-72, -47, 22, -3},
-        {-50, -22, -1, 20},
-        {-35,  3, 27, 39},
-        {-40, -2, 31, 45},
-        {-25, 15, 58, 42},
-        {-68, -37, -5, 30},
-        {-150,-84, -56, -24}
+        -136, -78, -61, -47,
+        -72, -47, 22, -3,
+        -50, -22, -1, 20,
+        -35,  3, 27, 39,
+        -40, -2, 31, 45,
+        -25, 15, 58, 42,
+        -68, -37, -5, 30,
+        -150,-84, -56, -24
     };
 }
 
@@ -36,9 +35,11 @@ public class Evaluator : IEvaluator
 
         static int psqIndex(int i)
         {
-            if (i < 4) return i;
-            return 7 - i;
+            int x = i >> 3, y = i % 8;
+            if (y >= 4) y = 7 - y;
+            return x * 4 + y;
         }
+        
         static int ColorV(bool color)
         {
             return color ? 1 : -1;
@@ -49,12 +50,11 @@ public class Evaluator : IEvaluator
         // Material
         var pieceList = board.GetAllPieceLists();
         foreach (var list in pieceList)
-        {
             foreach (var piece in list)
             {
                 materialScore += Values.pieceValues[(int)piece.PieceType] * ColorV(piece.IsWhite);
             }
-        }
+        
         
         // Mobility
         int mobilityScore = 0;
@@ -80,35 +80,28 @@ public class Evaluator : IEvaluator
                 Square square = new Square(i);
                 if (!stm) nullMove(); // set stm to white
                 if (board.SquareIsAttackedByOpponent(square))
-                {
                     BitboardHelper.ClearSquare(ref spaceMaskWhite, square); // set to 0 if unsafe
-                }
 
                 if (stm) nullMove(); // set stm to black
                 if (board.SquareIsAttackedByOpponent(square))
-                {
                     BitboardHelper.ClearSquare(ref spaceMaskBlack, square); // set to 0 if unsafe
-                }
                 
                 undoNullMove();
             }
             
             // count number of safe squares
             spaceScore = BitboardHelper.GetNumberOfSetBits(spaceMaskWhite) - BitboardHelper.GetNumberOfSetBits(spaceMaskBlack);
-            
         }
         
         // PSQT
         int psqtScore = 0;
         // knight
         foreach (bool color in new[] { true, false })
-        {
             foreach (var knight in board.GetPieceList(PieceType.Knight, color))
             {
                 int sq = knight.Square.Index, index = color ? sq : 63 - sq;
-                psqtScore += Values.knightPsqt[index >> 3, psqIndex(index % 8)] * ColorV(color);
+                psqtScore += Values.knightPsqt[psqIndex(index)] * ColorV(color);
             }
-        }
 
         int score = materialScore + mobilityScore + spaceScore + psqtScore;
         
