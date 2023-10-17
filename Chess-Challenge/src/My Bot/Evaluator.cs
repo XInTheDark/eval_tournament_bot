76ff5c178ef1c70ec86312dc8f8371afba6cc892
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-17 23:23:46 +0800

    Try to pack psqt.

--- Chess-Challenge/src/My Bot/Evaluator.cs
+++ Chess-Challenge/src/My Bot/Evaluator.cs
@@ -1,75 +1,59 @@
 using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Values
 {
     // null, pawn, knight, bishop, rook, queen, king
     public static int[] pieceValues = { 0, 126, 781, 781, 1276, 2538, 40000 };
     
-    // Psqt
-    public static int[,] allPsqt = new int[,]
+    // Packed Psqt
+    public static decimal[,] packedPsqt = new decimal[,]
     {
-        // 0 - Pawn
-        {
-            0, 0, 0, 0,
-            -4, -1, 10, 16,
-            -9, -15, 17, 23,
-            2, -15, 8, 29,
-            11, 2, -4, 7,
-            14, 4, 7, 18,
-            -5, 8, -7, -3,
-            0, 0, 0, 0
-        },
-        
-        // 1 - Knight
-        {
-            -136, -78, -61, -47,
-            -72, -47, 22, -3,
-            -50, -22, -1, 20,
-            -35, 3, 27, 39,
-            -40, -2, 31, 45,
-            -25, 15, 58, 42,
-            -68, -37, -5, 30,
-            -150, -84, -56, -24
-        },
-        
-        // 2 - Rook
-        // {
-        //     -21, -17, -12, -7,
-        //     -16, -11, -4, 2,
-        //     -7, -9, -1, 1,
-        //     -9, -2, -6, 0,
-        //     -14, -5, 2, -2,
-        //     -8, 0, 0, 11,
-        //     1, 8, 18, 14,
-        //     -10, -18, 9, 11
-        // },
-        
-        // 2 - King
-        {
-            271, 327, 271, 198,
-            278, 303, 234, 164,
-            194, 233, 164, 91,
-            164, 202, 133, 61,
-            154, 179, 105, 70,
-            123, 145, 81, 31,
-            88, 120, 65, 13,
-            0, 0, 0, 0
-        }
+        {4034157052646293636858773504m, 2789001439998792313019365633m, 4277994750m}, // pawn (1)
+        {3404287441579403956711512508m, 6534281595731031271123255791m, 17646465313090236126m}, // knight (2)
+        {1194703523763084064389366m, 1547429716061480218496204796m, 361686527623365632m}, // rook (4)
+        {7167995988612654671579075140m, 2500229339330604076760118057m, 51387926m}, // king (6)
     };
 }
 
 public class Evaluator : IEvaluator
 {
+    public sbyte[] PawnPsqt, KnightPsqt, RookPsqt, KingPsqt;
+
+    public void extract(int pc, ref sbyte[] psqt)
+    {
+        var decimals = new List<decimal>();
+        for (int i = 0; i < 4; i++)
+            decimals.Add(Values.packedPsqt[i, pc]);
+        psqt = decimals.SelectMany(x => decimal.GetBits(x).Take(3).SelectMany(y => BitConverter.GetBytes(y).Select(z => (sbyte)z))).ToArray();
+    }
+    public void Init()
+    {
+        // init PSQT
+        // PSQT: extract from packed
+        extract(0, ref PawnPsqt);
+        extract(1, ref KnightPsqt);
+        extract(2, ref RookPsqt);
+        extract(3, ref KingPsqt);
+    }
+    
+    public int Query(int pc, int sq)
+    {
+        sbyte[] psqt = pc switch
+        {
+            0 => PawnPsqt,
+            1 => KnightPsqt,
+            2 => RookPsqt,
+            3 => KingPsqt,
+        };
+        return psqt[sq];
+    }
+    
     public int Evaluate(Board board, Timer timer)
     {
-        int psqIndex(int i)
-        {
-            int y = i % 8;
-            if (y >= 4) y = 7 - y;
-            return i / 8 * 4 + y;
-        }
-        
+        Init();
         int ColorV(bool color)
         {
             return color ? 1 : -1;
@@ -78,7 +62,7 @@ public class Evaluator : IEvaluator
         // init variables
         var pieceList = board.GetAllPieceLists();
         bool stm = board.IsWhiteToMove, endgame = pieceList.Length <= 12;
-        int i = 0; // temp variable used to save tokens
+        int i = 0, j; // temp variable used to save tokens
         
         int materialScore = 0, mobilityScore = 0, spaceScore = 0, psqtScore = 0; 
         // init everything here to save tokens
@@ -95,11 +79,11 @@ public class Evaluator : IEvaluator
         foreach (var x in board.GetPieceList(pc, color))
         {
             i = x.Square.Index;
-            // j = color ? i : 63 - i;
-            psqtScore += Values.allPsqt[Math.Min((int)pc, 4) >> 1, psqIndex(color ? i : 63 - i)] * ColorV(color);
+            j = color ? i : 63 - i;
+            // Math.Min((int)pc, 4) >> 1, psqIndex(color ? i : 63 - i)
+            psqtScore += Query((int)pc == 6 ? 3 : (int)pc >> 1, j) * ColorV(color);
             // psqt types: 1 (pawn) -> 0, 2 (knight) -> 1, 6 (king) -> 2
         }
-        
         
         // Mobility -> skip in endgames
         if (!endgame)
