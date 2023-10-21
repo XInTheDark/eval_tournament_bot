06f9d231d2738d66001e81daca95152c9b42afa3
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-21 10:19:19 +0800

    Take 2: modified Ethereal values
    Using mg * 0.7 + eg * 0.3.

--- Chess-Challenge/src/My Bot/Evaluator.cs
+++ Chess-Challenge/src/My Bot/Evaluator.cs
@@ -12,7 +12,7 @@ public class Evaluator : IEvaluator
     
     /* VALUES */
     // null, pawn, knight, bishop, rook, queen, king
-    public static readonly int[] pieceValues = { 0, 150, 801, 852, 1307, 2581, 40000 };
+    public static readonly int[] pieceValues = { 0, 100, 441, 462, 680, 1391, 40000 };
     // bishop, rook, queen
     public static readonly int[] mobilityValues  = { 6, 5, 3, 0 };
     
