bca4108b1414f7a094c137803c3cff616015d08d
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-10-24 21:45:02 +0800

    Add tapered PSQT
    1. Add tapered eval. All PSQTs are directly taken from SF with 2x quantisation, except for king PSQT which was multiplied by 3/8 instead.
    Also, the phase variable is based on material, in order to save tokens.
    2. Removes open files & king centrality to save tokens.
    
    1+0.01:
    ```
    Score of test vs master: 130 - 93 - 25  [0.575] 248
    ...      test playing White: 70 - 40 - 14  [0.621] 124
    ...      test playing Black: 60 - 53 - 11  [0.528] 124
    ...      White vs Black: 123 - 100 - 25  [0.546] 248
    Elo difference: 52.2 +/- 41.6, LOS: 99.3 %, DrawRatio: 10.1 %
    SPRT: llr 0.484 (16.7%), lbound -2.25, ubound 2.89
    ```
    
    3+0.03:
    ```
    Score of test vs master: 160 - 129 - 47  [0.546] 336
    ...      test playing White: 80 - 55 - 33  [0.574] 168
    ...      test playing Black: 80 - 74 - 14  [0.518] 168
    ...      White vs Black: 154 - 135 - 47  [0.528] 336
    Elo difference: 32.1 +/- 34.6, LOS: 96.6 %, DrawRatio: 14.0 %
    SPRT: llr 0.428 (14.8%), lbound -2.25, ubound 2.89
    ```
    
    Tokens: 510

--- Chess-Challenge/src/My Bot/Evaluator.cs
+++ Chess-Challenge/src/My Bot/Evaluator.cs
@@ -7,14 +7,13 @@ using System.Linq;
 
 public class Evaluator : IEvaluator
 {
-    public static int[,] psqts = new int[6, 32]; // piece type, square
-    public int i, j, k, scoreAccum, rank, file; // temp variable used to save tokens
+    public static int[,] psqts = new int[12, 32]; // piece type, square
+    public int i, j, k, scoreAccum, phase, rank, file; // temp variable used to save tokens
     
     /* VALUES */
     public static readonly int[] evalValues  =
     {
         6, 5, 3, 0, // mobility
-        18, 1, 2, 26, 8, -13, // open file
         150, 801, 852, 1307, 2581, 40000 // material
     };
     
@@ -22,18 +21,24 @@ public class Evaluator : IEvaluator
     public static readonly decimal[] packedPsqt =
     {
         // x2 quantisation
-        4034157052646293636858773504m, 2789001439998792313019365633m, 4277994750m, // pawn (1)
-        2478254930630260639978739380m, 6520955114482203651610772206m, 17646465265845333982m, // knight (2)
-        1854520467773665165346274285m, 929677993905542949369152252m, 17940367219057883896m, // bishop (3)
-        309465972997065300676048884m, 1549842771690800342854663930m, 361125785249514752m, // rook (4)
-        933290603903513349640943094m, 928464345431493250755396094m, 18085039881020571383m, // queen (5)
-        21147051397208103401407871327m, 12137975184157770585622600265m, 799995959288806438m, // king (6)
+        7136274781365992494962049024m, 3707723598570256830967641853m, 4260890361m, // pawn MG
+        1853254782189190562434777088m, 8992052024079800685349501953m, 386923523m, // pawn EG
+        1861707429364259273588855464m, 8081725682095510024512341230m, 17574407607416976094m, // knight MG
+        4638586610520180804939145168m, 2776802959713810901776138222m, 17862636003402377181m, // knight EG
+        2164014940775567779109404397m, 1237954096498980378498368766m, 17940934567057882106m, // bishop MG
+        1237939928675561602158949868m, 620192910078788203406491385m, 17866890159820110325m, // bishop EG
+        618946168289982142167512816m, 1861745559436170078891998713m, 360278036081805055m, // rook MG,
+        78609173678942687852763019771m, 1852074429221265318044434685m, 434878881917960706m, // rook EG,
+        935727437387641066513890561m, 1241595114195444967460307458m, 18374967950420804605m, // queen MG,
+        614096186869342314756105181m, 303402252025252617224912628m, 17287883952131207911m, // queen EG,
+        14003442467243394413613120101m, 3440858740848085864472069949m, 18379226457874902305m, // king MG,
+        20192915250973596020264341504m, 22057154442866270888433564198m, 2097294258731691281m, // king EG
     };
     
     public Evaluator() // init
     {
         // init psqts - extract from packed
-        for (; i < 6; i++) // i=0 since this is the constructor
+        for (; i < 12; i++) // i=0 since this is the constructor
         {
             /* extract */
             var decimals = new List<decimal>();
@@ -56,18 +61,23 @@ public class Evaluator : IEvaluator
             pieceCount = GetNumberOfSetBits(board.AllPiecesBitboard);
 
         // Material, PSQT & mobility
+        int mgScore = 0, egScore = 0;
+        
         foreach (bool color in new[] { true, false })
         {
+            int mg = 0, eg = 0;
             ulong pawnBB = board.GetPieceBitboard(PieceType.Pawn, !color); // opponent's pawns
             
-            scoreAccum = k = 0; while (k++ < 6) // piece type, k -> 1 to 6
+            scoreAccum = k = 0;
+            while (k++ < 6) // piece type, k -> 1 to 6
             {
                 ulong bitboard = board.GetPieceBitboard((PieceType)k, color);
 
                 while (bitboard != 0) // iterate over every piece of that type
                 {
                     /* Material */
-                    scoreAccum += evalValues[k + 9];
+                    scoreAccum += evalValues[k + 3];
+                    phase += evalValues[k + 3];
 
                     i = ClearAndGetIndexOfLSB(ref bitboard); // square
 
@@ -85,18 +95,8 @@ public class Evaluator : IEvaluator
                     /* PSQT */
                     if (!color) i ^= 56; // flip square if black
                     rank = i >> 3; file = i & 7;
-                    scoreAccum += psqts[k - 1, // piece type
-                        rank * 4 + Min(file, 7 - file) // map square to psqt index
-                    ];
-
-                    /* late endgame: incentivize king moving towards center */
-                    if (pieceCount <= 14 && k == 6)
-                        scoreAccum -= (26 - pieceCount) * (Abs(4 - rank) + Abs(4 - file));
-                    
-                    /* Open files, doubled pawns */
-                    if ((0x101010101010101UL << file & ~(1UL << i) &
-                         board.GetPieceBitboard(PieceType.Pawn, color)) == 0)
-                        scoreAccum += evalValues[3 + k];
+                    j = rank * 4 + Min(file, 7 - file);
+                    mg += psqts[k * 2 - 2, j]; eg += psqts[k * 2 - 1, j];
                     
                     /* Passed Pawn */
                     // basic detection
@@ -106,10 +106,16 @@ public class Evaluator : IEvaluator
                             scoreAccum += rank * 224 / pieceCount;
                 } // piece bitboard loop
             } // piece type loop
-            
-            score += scoreAccum * (color ? 1 : -1);
-        } // color loop
 
+            j = color ? 1 : -1;
+            score += scoreAccum * j;
+            mgScore += mg * j; egScore += eg * j;
+            
+        } // color loop
+        
+        phase = phase / 75 - 1067; // (phase - 80000) / 75;
+        score += (mgScore * phase + egScore * (256 - phase)) / 256; // mgScore, egScore are from white POV
+        
         /* STM */
         score *= stm ? 1 : -1;
         
